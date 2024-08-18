d7f116edf8d31a8e6eab5c41cbe74550a0fe304a
sujin-git-hub <sujinc0@gmail.com>
2024-08-18 18:41:55 +0530

    Mahnifi

--- src/Pages/SingleProduct.jsx
+++ src/Pages/SingleProduct.jsx
@@ -1,6 +1,6 @@
 
 import { Link, useLoaderData } from 'react-router-dom';
-import customFeatch, { formatPrice, generateAmountOptions } from '../Utils'
+import customFeatch, { formatPrice, generateAmountOptions, magnifi } from '../Utils'
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../Features/Cart/cartSlice';
@@ -49,11 +49,11 @@ const SingleProduct = () => {
       {/* PRODUCT */}
       <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
         {/* IMAGE */}
-        <img
-          src={image}
-          alt={title}
-          className='w-96 h-96 object-cover rounded-lg lg:w-full  '
-        />
+        <div className='magnifi' style={{position : "relative"}} onMouseMove={magnifi}>
+          <div className="large_magnifi" style={{"display" : "none","width":"100px","height":"100px","position":"absolute","border":"3px solid #0d1b48","borderRadius":"50%"}}>
+          </div>
+          <img src={image} alt={title} className='w-96 h-96 object-cover rounded-lg' style={{margin:"auto"}}/>
+        </div>
         {/* PRODUCT INFO */}
         <div>
           <h1 className='capitalize text-3xl font-bold'>{title}</h1>
--- src/Utils/index.jsx
+++ src/Utils/index.jsx
@@ -27,4 +27,43 @@ export const generateAmountOptions = (number) => {
   });
 };
 
- export default customFeatch;
+export const magnifi = () => {
+  let box = event?.target;
+  if (!box) return;
+  if (box.nodeName == "IMG" || box.className == "large_magnifi") {
+    box = box.nodeName == "IMG" ? box : box.nextElementSibling;
+  }
+  
+  var imageURL = box.getAttribute('src');
+  
+  let image_object = new Image();
+  image_object.src = imageURL;
+  let native_width = image_object.width;
+  let native_height = image_object.height;
+  var magnify_offset = box.parentElement.getBoundingClientRect();
+  var mx = event.pageX - (magnify_offset.left + box.offsetLeft);
+  var my = event.pageY - (magnify_offset.top + box.offsetTop);
+  let large = document.getElementsByClassName("large_magnifi")[0];
+  if (mx < box.width && my < box.height && mx > 0 && my > 0) {
+    large.style.display = 'block';
+  } else {
+    large.style.display = 'none';
+  }
+  if (large.style.display == 'block') {
+    var rx =
+    Math.round((mx / box.width) * native_width - 100/2) * -1;
+    var ry =
+    Math.round((my / box.height) * native_height - 100/2) * -1;
+    var bgp = rx + "px " + ry + "px";
+    
+    var px = (mx + box.offsetLeft) - 100 / 2;
+    var py = (my + box.offsetTop) - 100 / 2;
+    large.style.background = `transparent url("${imageURL}") no-repeat`;
+    large.style.left = px + 'px';
+    large.style.top = py + 'px';
+    large.style.backgroundPosition = bgp;
+  }
+  //}
+}
+
+export default customFeatch;
